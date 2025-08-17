987d1a99cf86d03dffb35310ad766165190d05e9
Danil <voidilov@gmail.com>
2025-08-17 14:03:38 +0300

    update firebase version

--- Package.swift
+++ Package.swift
@@ -5,19 +5,20 @@ import PackageDescription
 
 let package = Package(
     name: "SwiftFirebaseTools",
+		platforms: [.iOS(.v15), .macCatalyst(.v15), .macOS(.v10_15), .tvOS(.v15), .watchOS(.v7)],
     products: [
         .library(name: "SwiftFirebaseTools", targets: ["SwiftFirebaseTools"]),
     ],
     dependencies: [
         .package(url: "https://github.com/dankinsoid/swift-analytics.git", from: "1.10.0"),
         .package(url: "https://github.com/dankinsoid/swift-configs.git", from: "0.11.0"),
-        .package(url: "https://github.com/firebase/firebase-ios-sdk.git", from: "10.0.0"),
+        .package(url: "https://github.com/firebase/firebase-ios-sdk.git", from: "12.0.0"),
     ],
     targets: [
         .target(
             name: "SwiftFirebaseTools",
             dependencies: [
-                .product(name: "FirebaseAnalyticsSwift", package: "firebase-ios-sdk"),
+                .product(name: "FirebaseAnalyticsCore", package: "firebase-ios-sdk"),
                 .product(name: "FirebaseRemoteConfig", package: "firebase-ios-sdk"),
                 .product(name: "FirebasePerformance", package: "firebase-ios-sdk"),
                 .product(name: "SwiftAnalytics", package: "swift-analytics"),
